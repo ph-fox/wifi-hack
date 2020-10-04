080278073232897fd20c14f5f494ac4b9b0f1af7
abalesluke <47096239+abalesluke@users.noreply.github.com>
2020-10-04 07:19:48 -0700

    Update py.py

--- py.py
+++ py.py
@@ -5,7 +5,7 @@ def update():
   print("updating!")
   os.system("rm -rf wifi.py")
   os.system("rm -rf con")
-  os.system("git clone https://github.com/abalesluke/wifi-hack/wifi.py")
-  os.system("git clone https://github.com/abalesluke/wifi-hack/con")
+  os.system("cd ..")
+  os.system("git clone https://github.com/abalesluke/wifi-hack")
   print("Updated successfully!")  
   
